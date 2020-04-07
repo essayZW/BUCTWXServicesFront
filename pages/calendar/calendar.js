b23a68aaa2f75ec432ec781338cf4456c73324be
essay <dazw2000@163.com>
2020-04-08 00:19:02 +0800

    真实日历的构建

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -27,12 +27,12 @@ Page({
      */
     onShow: function () {
         let weekdatList = ['日', '一', '二', '三', '四', '五', '六'];
-        let monthdayList = [];
         let DateObj = new Date();
+        let monthdayList = this.createCalendar(DateObj.getFullYear(), DateObj.getMonth());
         this.setData({
             'year' : DateObj.getFullYear(),
-            'month' : DateObj.getMonth() + 1,
-            'weekday' : DateObj.getDay(),
+            'month' : DateObj.getMonth(),
+            'weekday' : weekdatList[DateObj.getDay()],
             'day' : DateObj.getDate(),
             'weekdayList' : weekdatList,
             'monthdayList' : monthdayList
@@ -77,6 +77,34 @@ Page({
      * 生成一个指定年月份的日历表
      */
     createCalendar : function(year, month) {
-
+        // 获得当月的天数
+        let dayNum = new Date(year, month + 1, 0).getDate();
+        // 获得上个月的天数
+        let lastDayNum = new Date(Date.UTC(year, month, 0)).getDate();
+        // 获得本月的第一天的星期
+        let firstWeekday = new Date(Date.UTC(year, month, 1)).getDay();
+        let calendarRes = new Array(35);
+        for(let i = firstWeekday; i < dayNum + firstWeekday; i ++) {
+            calendarRes[i] = {
+                'day' : i - firstWeekday + 1,
+                'shortInfo' : '放假',
+                'inThisMonth' : true
+            };
+        }
+        for(let i = firstWeekday - 1; i >= 0; i --) {
+            calendarRes[i] = {
+                'day' : lastDayNum --,
+                'shortInfo' : '放假中',
+                'inThisMonth' : false
+            }
+        }
+        for(let i = firstWeekday + dayNum; i < 35; i ++) {
+            calendarRes[i] = {
+                'day' : i - firstWeekday - dayNum + 1,
+                'shortInfo' : '放假',
+                'inThisMonth' : false
+            }
+        }
+        return calendarRes;
     }
 })
