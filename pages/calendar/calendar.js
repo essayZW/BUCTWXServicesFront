0746bce275d39b19c70ea1456de8c1cab9bbf210
essay <dazw2000@163.com>
2020-04-23 19:32:04 +0800

    进行初步的全局主题切换适配

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,4 +1,5 @@
 // pages/calendar/calendar.js
+const App = getApp();
 Page({
 
     /**
@@ -15,6 +16,11 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        // 修改本页面顶栏颜色
+        wx.setNavigationBarColor({
+            backgroundColor: App.globalData.backgroundColor,
+            frontColor: App.globalData.frontColor
+        });
         // 实例化触摸类
         let touchObj = require('/../../utils/touch.js');
         touchObj = new touchObj(this.lastMonth, this.nextMonth)
@@ -39,6 +45,11 @@ Page({
     onShow: function () {
         let DateObj = new Date();
         this.changeDay(DateObj.getFullYear(), DateObj.getMonth(), DateObj.getDate());
+        // 设置该页面主题色
+        this.setData({
+            'globalBackgroundTheme' : App.globalData.backgroundColor,
+            'globalFronrTheme' : App.globalData.frontColor
+        });
     },
 
     /**
