d531f9458c9bb8f1787e69b4f46ae704340c1f71
essay <dazw2000@163.com>
2020-04-24 15:15:26 +0800

    修正错别字 & 修改设置页面顶栏主题的方法调用

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 //app.js
 App({
     onLaunch: function () {
-        // 读取本地存储的全局主题颜色
-        const AppConfig = require('/utils/config.js');
-        if(AppConfig.has('theme')) {
-            let colorSettings = AppConfig.get('theme');
-            this.globalData.frontColor = colorSettings.frontColor;
-            this.globalData.backgroundColor = colorSettings.backgroundColor;
-        }
+        
     },
     onShow: function() {
 
@@ -21,6 +15,38 @@ App({
     globalData: {
        frontColor : '#ffffff',
        backgroundColor : '#ffd21e'
+    },
+    /**
+     * 更新颜色信息
+     */
+    updateTheme : function() {
+        // 读取本地存储的全局主题颜色
+        const AppConfig = require('/utils/config.js');
+        if(AppConfig.has('theme')) {
+            let colorSettings = AppConfig.get('theme');
+            this.globalData.frontColor = colorSettings.frontColor;
+            this.globalData.backgroundColor = colorSettings.backgroundColor;
+        }
+    },
+    /**
+     * 设置顶栏颜色
+     */
+    setNavigatorColor : function() {
+        // 修改本页面顶栏颜色
+        wx.setNavigationBarColor({
+            backgroundColor: this.globalData.backgroundColor,
+            frontColor: this.globalData.frontColor
+        });
+    },
+    /**
+     * 设置对应页面的主题颜色
+     */
+    setPageColor : function(page) {
+        // 设置该页面主题色
+        page.setData({
+            'globalBackgroundTheme' : this.globalData.backgroundColor,
+            'globalFrontTheme' : this.globalData.frontColor
+        });
     }
 })
 
--- pages/notice/notice.js
+++ pages/notice/notice.js
@@ -14,10 +14,7 @@ Page({
    */
   onLoad: function (options) {
     // 修改本页面顶栏颜色
-    wx.setNavigationBarColor({
-      backgroundColor: App.globalData.backgroundColor,
-      frontColor: App.globalData.frontColor
-    });
+    App.setNavigatorColor();
   },
 
   /**
@@ -32,10 +29,7 @@ Page({
    */
   onShow: function () {
     // 设置该页面主题色
-    this.setData({
-      'globalBackgroundTheme' : App.globalData.backgroundColor,
-      'globalFronrTheme' : App.globalData.frontColor
-    });
+    App.setPageColor(this);
   },
 
   /**
