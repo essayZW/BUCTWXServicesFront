c8e06c7822587bf168b7261038063a0006d1059f
essay <dazw2000@163.com>
2020-07-06 19:34:23 +0800

    迎合官方审核

--- app.js
+++ app.js
@@ -3,6 +3,43 @@ const AppConfig = require('/utils/config.js');
 const token = require('/utils/token.js');
 App({
     onLaunch: function () {
+        // 更新
+        const updateManager = wx.getUpdateManager()
+
+        updateManager.onCheckForUpdate(function (res) {
+            // 请求完新版本信息的回调
+            console.log(res.hasUpdate)
+        })
+
+        updateManager.onUpdateReady(function () {
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success(res) {
+                    if (res.confirm) {
+                        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                        updateManager.applyUpdate()
+                    }
+                    else if(res.cancel) {
+                        // 没有选择更新
+                        wx.showToast({
+                          title: '若不正常更新可能部分功能无法使用',
+                          icon: "none"
+                        });
+                    }
+                }
+            })
+        });
+
+        updateManager.onUpdateFailed(function () {
+            // 新版本下载失败
+            wx.showToast({
+              title: '更新失败',
+              image:"/images/icon/error.png",
+              duration: 700
+            });
+        });
+
         this.reset();
         this.updateTheme();
         if(AppConfig.has('userheadInfo')) {
